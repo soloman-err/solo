7a7364e84d49b9b1789fc28fb99c67d81d70ee0e
solo <soloman.err@.com>
2023-09-28 09:30:32 +0600

    about info integrated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,6 +4,7 @@ import {
   FaLinkedin,
   FaTwitterSquare,
 } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import './About.scss';
 
 const About = () => {
@@ -16,7 +17,7 @@ const About = () => {
           <img src="/soloico.jpg" alt="" />
           <p>
             <span>
-              Being enthusiastic doesn't mean doing whatever you come across. It
+              Being enthusiastic does not mean doing whatever you come across. It
               stems from the inner depths of passion.
             </span>
             <br />
@@ -25,19 +26,36 @@ const About = () => {
             prioritize responsive design. With expertise in: HTML5 | CSS3 |
             Bootstrap | Tailwind | JavaScript | Node | React 💻
             <br />
-            Let's collaborate to bring your vision to life with my seasoned
+            Lets collaborate to bring your vision to life with my seasoned
             skills and unwavering passion for excellence. Together, we can
             create outstanding web solutions that leave a lasting impact.
             Connect with me today!
           </p>
 
           <div className="social-container">
-            <div className='line'></div>
-            <div className='social'>
-              <FaGithubSquare size={28} />
-              <FaTwitterSquare size={28} />
-              <FaDiscord size={28} />
-              <FaLinkedin size={28} />
+            <div className="line">{'/'}</div>
+            <div className="social">
+              <Link to={'https://github.com/soloman-err'} target="_blank">
+                <FaGithubSquare size={28} />
+              </Link>
+
+              <Link to={'https://twitter.com/soloman_err'} target="_blank">
+                <FaTwitterSquare size={28} />
+              </Link>
+
+              <Link
+                to={'https://discord.com/users/1087367193961955459'}
+                target="_blank"
+              >
+                <FaDiscord size={28} />
+              </Link>
+
+              <Link
+                to={'https://www.linkedin.com/in/soloman-err/'}
+                target="_blank"
+              >
+                <FaLinkedin size={28} />
+              </Link>
             </div>
           </div>
         </div>
